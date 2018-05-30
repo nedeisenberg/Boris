7e263f19a44120b24525df9769c743250e59509d
ned eisenberg <ned.eisenberg@gmail.com>
2018-05-30 16:20:45 -0400

    testing css linkage

--- WP_Scripts/transcript2vtt.js
+++ WP_Scripts/transcript2vtt.js
@@ -1,6 +1,14 @@
 video_column = document.createElement('div');
 video_column.setAttribute('id','doesntmatter');
+video_column.classList.add('column_third');
 video_column.innerHTML = 'bonjour';
 
+second_column = video_column.cloneNode(true);
+
+second_column.innerHTML= 'bon nuit';
+
+
+
 //attach content to hook
-jQuery("#content").append(video_column);
+jQuery("#content").appendChild(video_column);
+jQuery("#content").appendChild(second_column);
