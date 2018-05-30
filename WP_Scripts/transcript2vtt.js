37fad02a5f55e3f4da74553b076b627729cad941
ned eisenberg <ned.eisenberg@gmail.com>
2018-05-30 14:33:18 -0400

    you need 'jQuery' instead of '$'

--- WP_Scripts/transcript2vtt.js
+++ WP_Scripts/transcript2vtt.js
@@ -3,4 +3,4 @@ video_column.setAttribute('id','doesntmatter');
 video_column.innerHTML = 'bonjour';
 
 //attach content to hook
-$("#content").append(video_column);
+jQuery("#content").append(video_column);
