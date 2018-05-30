86396b7ba392c837e8dbc2e0ff745c6074dd939f
ned eisenberg <ned.eisenberg@gmail.com>
2018-05-30 14:03:26 -0400

    testing updates

--- WP_Scripts/transcript2vtt.js
+++ WP_Scripts/transcript2vtt.js
@@ -1,4 +1,6 @@
 video_column = document.createElement('div');
-video_column.setAttribute('id','hook_tag');
-video_column.innerHTML = "check mate";
+video_column.setAttribute('id','doesntmatter');
+video_column.innerHTML = 'bonjour';
+
+//attach content to hook
 jQuery("#content").append(video_column);
