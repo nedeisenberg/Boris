16323c8918c59094be01b0fa7ff0f67d7b1ce3ff
ned eisenberg <ned.eisenberg@gmail.com>
2018-05-30 17:01:43 -0400

    t2v construction

--- WP_Scripts/transcript2vtt.js
+++ WP_Scripts/transcript2vtt.js
@@ -7,7 +7,15 @@ video_column.classList.add('column_third');
 transcript_column = video_column.cloneNode(true);
 vtt_column = video_column.cloneNode(true);
 
-container.appendChild([video_column,transcript_column,vtt_column]);
+container.appendChild(video_column);
+container.appendChild(transcript_column);
+container.appendChild(vtt_column);
+
+title = document.createElement('div');
+title.innerHTML = 'check';
+
+video_column.appendChild(title);
+)
 
 
 //attach content to hook
