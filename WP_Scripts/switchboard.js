9b27613fd5220d09f8729eeef82eecea318bbf8d
ned eisenberg <ned.eisenberg@gmail.com>
2018-06-08 14:48:09 -0400

    update

--- WP_Scripts/switchboard.js
+++ WP_Scripts/switchboard.js
@@ -8,6 +8,10 @@ switch(script_name){
       jQuery('#content').children().hide();
       loadScript.src = 'https://nedeisenberg.github.io/Boris/WP_Scripts/transcript2vtt.js';
       break;
+   case "videoScroller":
+      jQuery('#content').children().hide();
+      loadScript.src = 'https://nedeisenberg.github.io/Boris/WP_Scripts/videoScroller.js';
+      break;
    case "pdfOCR":
       jQuery('#content').children().hide();
       loadScript.src = 'https://nedeisenberg.github.io/Boris/WP_Scripts/pdf_display.js';
