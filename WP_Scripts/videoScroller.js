4bfab98c3b0bb8138fdbd9b092363b836b29e13a
ned eisenberg <ned.eisenberg@gmail.com>
2018-06-08 14:53:19 -0400

    checking applet hook

---
+++ WP_Scripts/videoScroller.js
@@ -0,0 +1,8 @@
+container = document.createElement('div');
+container.classList.add('app_container');
+
+container.innerHTML = 'Hello video scroller';
+
+
+//attach content to hook
+jQuery("#content").append(container);
