043013c1ee20163fae42b6460e36f90d140ffcb3
ned eisenberg <ned.eisenberg@gmail.com>
2018-06-08 15:01:38 -0400

    added columns

--- WP_Scripts/videoScroller.js
+++ WP_Scripts/videoScroller.js
@@ -1,8 +1,16 @@
 container = document.createElement('div');
 container.classList.add('app_container');
 
-container.innerHTML = 'Hello video scroller';
+leftColumn = document.createElement('div');
+rightColumn = document.createElement('div');
+leftColumn.classList.add('column_half');
+rightColumn.classList.add('column_half');
 
+leftColumn.innerHTML = 'video and controls';
+rightColumn.innerHTML = 'transcript';
+
+container.appendChild(leftColumn);
+container.appendChild(rightColumn);
 
 //attach content to hook
 jQuery("#content").append(container);
